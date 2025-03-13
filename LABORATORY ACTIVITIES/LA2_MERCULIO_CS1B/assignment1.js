10f8b0884a1999cb567afb179e0a42428bcc9a14
Trisha Mae Merculio <trishamae02005@gmail.com>
2025-03-13 13:56:18 +0800

    assignment1.js

--- LABORATORY ACTIVITIES/LA2_MERCULIO_CS1B/assignment1.js
+++ LABORATORY ACTIVITIES/LA2_MERCULIO_CS1B/assignment1.js
@@ -1,15 +1,38 @@
-//Persona 1
-//name = "Trisha Mae Merculio";
-let persona1 = {
-    fname: "trisha mae",
-    mname: "besting",
-    lname: "merculio",
-    bday: "february 02, 2005",
-    bplace: "palacapac, candon City, ilocos sur, 2710",
-    address: "palacapac, candon city, ilocos sur, 2710",
-    course: "bachelor of science in computer science",
-    year: "First year",
-    dreamJob: "Software Engineer"
-};
+//Personal 1
+let name= "trisha mae merculio"; //fullname of persona 1
+let bday = "february 02, 2005"; //birhtday of persona 1
+let bplace = "palacapac, candon city, ilocos sur, 2710"; //birthplace of persona 1
+let address =  "palacapac, candon city, ilocos Sur, 2710"; //address of persona 1
+let course =  "bachelor of science in computer science"; //course of persona 1
+let year = "first year"; //year
+let dreamJob = "software engineer"; //dream job of persona 1
 
-//console.log(`[${name}] was born [${bday}] at [${bplace}]`);
+//display the information of persona 1
+console.log(`[${name.toUpperCase()}] was born [${bday}] at [${bplace.toUpperCase()}], 
+and currently living at [${address.toUpperCase()}]. [${name}]  is taking up [${course}] and dreams to be [${dreamJob}] after graduation.`);
+
+//Persona 2
+let name_2 = "mitzie agila ponce"; // fullname of persona 2
+let bday_2 = "august 04, 2006"; // birthday of persona 2
+let bplace_2 = "bulbulala, santiago city, ilocos sur, philippines, 2707";
+let address_2 = "bulbulala, santiago city, ilocos sur, philippines, 2707";
+let course_2 = "bachelor of science in computer science";
+let year_2 = "first year";
+let dreamJob_2 = "Web Developer";
+
+//display the information of persona 2
+console.log(`[${name_2.toUpperCase()}] was born [${bday_2}] at [${bplace_2.toUpperCase()}], 
+and currently living at [${address_2.toUpperCase()}]. [${name}]  is taking up [${course_2}] and dreams to be [${dreamJob_2}] after graduation.`);
+
+//Persona 3
+let name_3 = "carl stephen quilopras"; //fullname of persona 3
+let bday_3 = "september 21, 2005";
+let bplace_3 = "daldagan, galimuyod city, ilocos sur, 2709";
+let address_3 = "daldagan, galimuyod city, ilocos sur, 2709";
+let course_3 = "bachelor of science in computer science";
+let year_3 = "first year";
+let dreamJob_3 = "Software Enginer";
+
+//display the information of personal 3
+console.log(`[${name_3.toUpperCase()}] was born [${bday_2}] at [${bplace_3.toUpperCase()}], 
+and currently living at [${address_3.toUpperCase()}]. [${name}]  is taking up [${course_3}] and dreams to be [${dreamJob_3}] after graduation.`);
