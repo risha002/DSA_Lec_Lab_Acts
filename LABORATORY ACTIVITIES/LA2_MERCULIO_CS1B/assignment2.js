8ff7ad6e6bf8fa1d3354a13c78ef1b105e7fc372
Trisha Mae Merculio <trishamae02005@gmail.com>
2025-03-13 13:27:09 +0800

    assignment2.js

---
+++ LABORATORY ACTIVITIES/LA2_MERCULIO_CS1B/assignment2.js
@@ -0,0 +1,25 @@
+let a = 25;
+let b = 20;
+let c = 12;
+let d = "15";
+const E = 15;
+
+//a. display the sum of the declared variables (a,b,c,c) and constant (e) combined.
+console.log(a + b + c + d + E);
+//b.
+console.log(d > E); //is variable d greater than variable E?
+console.log(d < E); // is var d less than var E?
+console.log(d >= E); //is variable d greater or equal than variable E?
+console.log(d <= E); //is variable d less than or equal than variable E?
+console.log(d === E);
+
+//c.
+//1.
+console.log(a - b - c - d - E); //subtract all the values of the declared var
+//2.
+console.log(a * c / E); //multiply the values of var a and c, divided by const E
+//3.
+console.log(E ** c); // exponent value of const E raise to the power of var c
+//4. 
+console.log(c = E);
+
