d2b210c4779d182b29d5df0194641bfaf00718ec
Trisha Mae Merculio <trishamae02005@gmail.com>
2025-03-28 13:11:08 +0800

    Add files via upload

---
+++ LABORATORY ACTIVITIES/Perf_3_MERCULIO_CS1B/arrayActivity.js
@@ -0,0 +1,17 @@
+//create an array of my favorite singers
+let favSinger = ["Seventeen", "Arian Grande", "Niki"];
+//log the first singer in the array
+console.log(favSinger[0]);
+
+//create an array of my favorite numbers
+let favNumbers = [2, 5, 21, 14];
+
+//create an array with mixed data types
+//                 0            1          2    3
+let mixedArr = ["String", ["otherarray"], 123, true];
+
+//print
+console.log(mixedArr[0]); //output: "string"
+console.log(mixedArr[1]); //output: ["otherarray"]
+console.log(mixedArr[2]); //output: 123
+console.log(mixedArr[3]); //output: true
