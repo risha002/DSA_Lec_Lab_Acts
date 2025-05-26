078aeb09906045699c2415ee7bad2cbef61632e8
Trisha Mae Merculio <trishamae02005@gmail.com>
2025-05-26 12:24:18 +0800

    Update sortArray.js

--- CS1B_FINExam_Merculio/sortArray.js
+++ CS1B_FINExam_Merculio/sortArray.js
@@ -1,19 +1,17 @@
-/*
-let numbers = parent(prompt("Enter some numbers: "));
-let names = prompt("Enter name: ");
+function arrayOperation() {
+    let numbersInput = prompt("Enter numbers sperated by comma: "); //prompt the user to input numbers seperated by comma
+    let namesInput = prompt("Enter names seperated with comma: "); //prompt the user to input names seperated by comma
 
-function reverseNum(int) {
-    return 
+    let numbers = numbersInput.split(",").map(number => parseFloat(number.trim())); //convert input strings to array of numbers
+    let names = namesInput.split(",").map(name => name.trim()); //convert input strings to array of names
+
+    let mergedArray = numbers.concat(names); //merge both arrays
+    console.log("Merged arrays: ", mergedArray);
+
+    let sortNumbers = numbers.slice().sort((a, b) => b - a); //sort numbers numerically in reverse order
+    console.log("Numbers sorted in reverse numerical order: ", sortNumbers);
+
+    let sortedNames = names.slice().sort(); //sort names alphabetically
+    console.log("Names sorted alphabetically: ", sortedNames);
 }
-*/
-
-/*
-let input = prompt("Enter a list of items separated by commas:");
-let array = input.split(",");
-
-console.log(array);
-*/
-
-let numberInput = parseInt(prompt("Enter numbers: "));
-let namesInput = prompt("Enter names: ");
-let merged = numberInput.concat(namesInput); //merged both arrays
+arrayOperation();
